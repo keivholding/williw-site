4288c7a4803beec2467a3cc74855e41b16c861b5
Keiv Holding <holding.keiv@gmail.com>
2023-04-20 20:44:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const welcomeBox = document.querySelector(".welcome-box");
+const willowText = document.querySelector(".willow-text");
+
+window.addEventListener("load", function () {
+  welcomeBox.querySelectorAll(".child").forEach((child, index) => {
+    setTimeout(() => {
+      child.classList.add("open");
+    }, 300 * index);
+  });
+});
