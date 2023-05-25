91731c8ef9c558a1a0b9b218df50392e62b7ae56
Keiv Holding <holding.keiv@gmail.com>
2023-05-25 16:36:06 -0400

    chatbox

--- chatbox.js
+++ chatbox.js
@@ -1,3 +1,4 @@
+// Adds padding to the very last message so that the input / user message field doesn't hide the text
 const addPaddingToBottomMessage = function () {
   const allMessages = document.querySelectorAll(".message-container");
 
@@ -7,3 +8,11 @@ const addPaddingToBottomMessage = function () {
 };
 
 addPaddingToBottomMessage();
+
+const userInput = document.querySelector(".users-message");
+
+//
+userInput.addEventListener("input", function () {
+  this.style.height = "auto";
+  this.style.height = this.scrollHeight + "px";
+});
