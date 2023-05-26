8a09873c6053a3abded36a1c4637e9e653849b4e
Keiv Holding <holding.keiv@gmail.com>
2023-05-26 13:37:22 -0400

    accepting screenshots

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const welcomeBox = document.querySelector(".welcome-box");
 const willowText = document.querySelector(".willow-text");
 
+// Makes the popup effect --> adds open class on page load
 window.addEventListener("load", function () {
   welcomeBox.querySelectorAll(".child").forEach((child, index) => {
     setTimeout(() => {
